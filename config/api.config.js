6aa2b3d0d70e436852dd2da100036825a0b4bbe7
MemoryShadow <31596045+MemoryShadow@users.noreply.github.com>
2023-07-01 10:30:31 +0000

    fix: 404的情况, 使用了自己的应用ID

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  clientId: 'e4458d68-b1a6-42c6-bae5-cf45af634bfb',
+  obfuscatedClientSecret: 'U2FsdGVkX1/0klWp3BIHsC65K9HNFF8ZAdw6qBw6KE1JhINm5eSf8q+gzZvXezqTMaYAL3fokb7FP9/esbSu0w==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
